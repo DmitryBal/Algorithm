f957d76ad96fce47cb8303736247e3511c9f4231
DmitryBal <76563919+DmitryBal@users.noreply.github.com>
2025-06-30 15:53:52 +0300

    Create YoungestCommonAncestor.py

---
+++ middle/YoungestCommonAncestor.py
@@ -0,0 +1,32 @@
+class AncestralTree:
+    def __init__(self, name):
+        self.name = name
+        self.ancestor = None
+
+
+# O(d) time | O(1) space 
+def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
+    depthOne = getDescendantDepth(descendantOne, topAncestor)
+    depthTwo = getDescendantDepth(descendantTwo, topAncestor)
+    if depthOne > depthTwo:
+        return backtrackAncestralTree(descendantOne, descendantTwo, depthOne - dep
+        else:
+        return backtrackAncestralTree(descendantTwo, descendantOne, depthTwo - dep
+
+
+def getDescendantDepth(descendant, topAncestor):
+    depth = 0
+    while descendant != topAncestor:
+        depth += 1
+        descendant = descendant.ancestor
+    return depth
+
+
+def backtrackAncestralTree(lowerDescendant, higherDescendant, diff):
+    while diff > 0:
+        lowerDescendant = lowerDescendant.ancestor
+        diff -= 1
+    while lowerDescendant != higherDescendant:
+        lowerDescendant = lowerDescendant.ancestor
+        higherDescendant = higherDescendant.ancestor
+    return lowerDescendan
